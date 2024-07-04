71213d7cba7b89ce4f9d9516debeefcbde64da1a
Olivier Spinelli <olivier.spinelli@signature-code.com>
2024-07-04 15:09:09 +0200

    Upgraded. Error: CK.DB.Res.ResString.
    This reference is not preserved by CK.DB.WebPage.

--- Tests/CK.AspNet.SiteMap.Tests/SiteMapTests.cs
+++ Tests/CK.AspNet.SiteMap.Tests/SiteMapTests.cs
@@ -1,6 +1,7 @@
 using CK.Auth;
 using CK.Core;
 using CK.Testing;
+using FluentAssertions.Equivalency.Steps;
 using NUnit.Framework;
 using System.Reflection;
 using static CK.Testing.SqlServerTestHelper;
@@ -11,8 +12,19 @@ namespace CK.AspNet.SiteMap.Tests
     public partial class SiteMapTests
     {
         [Test]
-        public async Task test_SiteMap_Async()
+        public async Task AspNet_SiteMap_Async()
         {
+            var targetOutputPath = TestHelper.GetTypeScriptBuildModeTargetProjectPath();
+
+            var engineConfiguration = TestHelper.CreateDefaultEngineConfiguration();
+            engineConfiguration.FirstBinPath.Assemblies.Add( "CK.AspNet.SiteMap" );
+            engineConfiguration.FirstBinPath.EnsureTypeScriptConfigurationAspect( targetOutputPath );
+            TestHelper.EnsureSqlServerConfigurationAspect( engineConfiguration );
+
+            engineConfiguration.RunSuccessfully();
+
+            await using var runner = TestHelper.CreateTypeScriptRunner(targetOutputPath);
+
             //
             // When running in Debug, this will wait until resume is set to true.
             // Until then, the .NET server is running and tests can be manually executed
@@ -23,25 +35,10 @@ namespace CK.AspNet.SiteMap.Tests
             //
             // In regular run, this will not wait for resume.
             //
+            await TestHelper.SuspendAsync( resume => resume);
 
+            runner.Run();
 
-            // We must register CK.IO.Auth.Basic to have the IBasicLoginCommand and ILogoutCommand (there is no dependency on it).
-            // And we must also register CK.Cris.Auth to have the CrisAuthenticationService
-            var types = TestHelper.CreateTypeCollector( typeof( SiteMapCommandHandler ).Assembly );
-            // 
-            //                    .AddModelDependentAssembly( typeof( IBasicLoginCommand ).Assembly )
-            //                    .AddModelDependentAssembly( typeof( CrisAuthenticationService ).Assembly );
-
-            var engineConfiguration = TestHelper.CreateDefaultEngineConfiguration();
-            TestHelper.EnsureSqlServerConfigurationAspect( engineConfiguration );
-
-            var targetOutputPath = TestHelper.GetTypeScriptBuildModeTargetProjectPath();
-            Throw.DebugAssert( targetOutputPath.EndsWith( "/TSBuildAndTests/test_SiteMap" ) );
-            await TestHelper.RunSingleBinPathAspNetE2ETestAsync( engineConfiguration,
-                                                                 targetOutputPath,
-                                                                 registeredTypes: types,
-                                                                 tsTypes: Type.EmptyTypes,
-                                                                 resume => resume );
         }
     }
 }
